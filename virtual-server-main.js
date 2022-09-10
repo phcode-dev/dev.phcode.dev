0aad2c0ccb67b679034dd4c590b0a3e43ef7d6d7
phoenixide <phoenixide@users.noreply.github.com>
2022-09-10 16:21:59 +0000

    deploy: 851cfff8b3defac1e6c72e943951d28ef0d8865e

--- virtual-server-main.js
+++ virtual-server-main.js
@@ -28,6 +28,8 @@ importScripts('phoenix/virtualServer/html-formatter.js');
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js');
 
 const _debugSWCacheLogs = false; // change debug to true to see more logs
+const CACHE_FILE_NAME = "cacheManifest.json";
+const CACHE_FS_PATH = `/${CACHE_FILE_NAME}`;
 
 workbox.setConfig({debug: _debugSWCacheLogs && Config.debug});
 
@@ -41,16 +43,18 @@ const DAYS_30_IN_SEC = 60 * 60 * 24 * 30;
 const CACHE_REFRESH_SCHEDULE_TIME = 10 * 1000;
 const CACHE_NAME_EVERYTHING = "everything";
 const CACHE_NAME_CORE_SCRIPTS = "coreScripts";
+const CACHE_NAME_EXTERNAL = "external";
 const ExpirationManager ={
     "everything": new CacheExpiration(CACHE_NAME_EVERYTHING, {
             maxAgeSeconds: DAYS_30_IN_SEC
         }),
     "coreScripts": new CacheExpiration(CACHE_NAME_CORE_SCRIPTS, {
             maxAgeSeconds: DAYS_30_IN_SEC
-        })
+        }),
+    "external": new CacheExpiration(CACHE_NAME_EXTERNAL, {
+        maxAgeSeconds: DAYS_30_IN_SEC
+    })
 };
-
-let recentlyAccessedURLS = {};
 
 function _debugCacheLog(...args) {
     if(_debugSWCacheLogs){
@@ -58,21 +62,30 @@ function _debugCacheLog(...args) {
     }
 }
 
+function _removeParams(url) {
+    if(url.indexOf( "?")>-1){
+        url = url.substring( 0, url.indexOf( "?")); // remove query string params
+    }
+    if(location.href.indexOf( "#")>-1){
+        url = url.substring( 0, url.indexOf( "#")); // remove hrefs in page
+    }
+    return url;
+}
+
 // service worker controlling route base url. This will be something like https://phcode.dev/ or http://localhost:8000/
 let baseURL = location.href;
-if(location.href.indexOf( "?")>-1){
-    baseURL = location.href.substring( 0, location.href.indexOf( "?")); // remove query string params
-}
-if(location.href.indexOf( "#")>-1){
-    baseURL = baseURL.substring( 0, baseURL.indexOf( "#")); // remove hrefs in page
-}
+baseURL = _removeParams(location.href);
 if(location.href.indexOf( "/")>-1){
+    // http://phcode.dev/index.html -> http://phcode.dev
     baseURL = baseURL.substring( 0, baseURL.lastIndexOf( "/"));
 }
 if(!baseURL.endsWith('/')){
     baseURL = baseURL + '/';
 }
 console.log("Service worker: base URL is: ", baseURL);
+
+const CACHE_MANIFEST_URL = `${baseURL}${CACHE_FILE_NAME}`;
+console.log("Service worker: cache manifest URL is: ", CACHE_MANIFEST_URL);
 
 // this is the base url where our file system virtual server lives. http://phcode.dev/phoenix/vfs in phoenix or
 // http://localhost:8000/phoenix/vfs in dev builds
@@ -136,7 +149,7 @@ function _clearCache() {
     });
 }
 
-async function _updateTTL(cacheName, urls) {
+function _updateTTL(cacheName, urls) {
     // this is needed for workbox to purge cache by ttl. purge behaviour is not part of w3c spec, but done by workbox.
     // cache.addall browser api will not update expiry ttls that workbox lib needs. So we add it here.
     console.log(`Service worker: Updating expiry for ${urls.length} urls in cache: ${cacheName}`);
@@ -145,34 +158,108 @@ async function _updateTTL(cacheName, urls) {
     }
 }
 
-function _refreshCacheNow(cacheName) {
-    console.log("Service worker: Refreshing cache: ", cacheName);
-    caches.open(cacheName).then((cache) => {
-        cache.keys().then((keys) => {
-            let cacheURLS = [];
-            keys.forEach((request, index, array) => {
-                cacheURLS.push(request.url);
-            });
-            console.log(`Service worker: Filtering caching from ${cacheURLS.length} URLS in ${cacheName}`);
-            cacheURLS = cacheURLS.filter(url => recentlyAccessedURLS[url] === true);
-            recentlyAccessedURLS = {};
-            console.log(`Service worker: scheduling cache update for ${cacheURLS.length} filtered URLS in ${cacheName}`);
-            cache.addAll(cacheURLS).then(()=>{
-                console.log(`Service worker: cache refresh complete for ${cacheURLS.length} URLS in ${cacheName}`);
-                _updateTTL(cacheName, cacheURLS);
-            }).catch(err=>{
-                console.error(`Service worker: cache refresh failed for ${cacheURLS.length} URLS in ${cacheName}`, err);
-            });
+function _getCurrentCacheManifest() {
+    return new Promise((resolve)=>{
+        fs.readFile(CACHE_FS_PATH, "utf8", function (err, data) {
+            if (err) {
+                resolve(null);
+            } else {
+                resolve(JSON.parse(data));
+            }
         });
     });
 }
+function _putCurrentCacheManifest(manifestObject) {
+    return new Promise((resolve)=>{
+        fs.writeFile(CACHE_FS_PATH, JSON.stringify(manifestObject, null, 2), "UTF8", function (err) {
+            if (err) {
+                console.error("Service worker: Failed while writing cache manifest", err);
+            }
+            resolve(null);
+        });
+    });
+}
+function _getNewCacheManifest() {
+    return new Promise((resolve) => {
+        fetch(CACHE_MANIFEST_URL)
+            .then((response) => response.json())
+            .then((data) => resolve(data))
+            .catch(err =>{
+                console.error("Service worker: could not fetch cache manifest for app updates", err);
+                resolve(null);
+            });
+    });
+}
 
-function _refreshCache(event) {
-    console.log("Service worker: Scheduling Refreshing cache in ms:", CACHE_REFRESH_SCHEDULE_TIME);
-    setTimeout(()=>{
-        _refreshCacheNow(CACHE_NAME_EVERYTHING);
-    }, CACHE_REFRESH_SCHEDULE_TIME);
-    event.ports[0].postMessage("cache refresh scheduled");
+function _fixCache(currentCacheManifest, newCacheManifest) {
+    const currentCacheKeys = Object.keys(currentCacheManifest);
+    const newCacheKeys = Object.keys(newCacheManifest);
+    console.log(`Service worker: Fixing Stale Cache Entries in ${CACHE_NAME_EVERYTHING}. num cache entries in manifest:
+    current: ${currentCacheKeys.length} new: ${newCacheKeys.length}`);
+    return new Promise((resolve, reject) => {
+        caches.open(CACHE_NAME_EVERYTHING).then((cache) => {
+            cache.keys().then(async (keys) => {
+                console.log("Service worker: Number of cached entries in everything cache: ", keys.length);
+                let changedContentURLs = [], deletePromises = [];
+                keys.forEach((request, _index, _array) => {
+                    let relativeURL = _removeParams(request.url);
+                    relativeURL = relativeURL.substring(baseURL.length, relativeURL.length);
+                    if(!newCacheManifest[relativeURL]){
+                        _debugCacheLog("Service worker: entry renewed as deleted", relativeURL);
+                        deletePromises.push(cache.delete(request));
+                        return;
+                    }
+                    if(currentCacheManifest[relativeURL] !== newCacheManifest[relativeURL]){
+                        _debugCacheLog("Service worker: entry renewed as changed", relativeURL);
+                        deletePromises.push(cache.delete(request));
+                        changedContentURLs.push(request.url);
+                    }
+                });
+                console.log(`Service worker: deleting ${deletePromises.length} stale cache entries in ${CACHE_NAME_EVERYTHING}`);
+                await Promise.all(deletePromises);
+                console.log(`Service worker: updating cache for ${changedContentURLs.length} in ${CACHE_NAME_EVERYTHING}`);
+                cache.addAll(changedContentURLs).then(()=>{
+                    console.log(`Service worker: cache refresh complete for ${changedContentURLs.length} URLS in ${CACHE_NAME_EVERYTHING}`);
+                    _updateTTL(CACHE_NAME_EVERYTHING, changedContentURLs);
+                    resolve();
+                }).catch(err=>{
+                    console.error(`Service worker: cache refresh failed for ${changedContentURLs.length} URLS in ${CACHE_NAME_EVERYTHING}`, err);
+                    reject();
+                });
+            });
+        }).catch(reject);
+    });
+}
+
+let refreshInProgress = false;
+async function _refreshCache(event) {
+    if(refreshInProgress){
+        console.log("Another cache refresh is in progress, ignoring.");
+        return;
+    }
+    refreshInProgress = true;
+    try{
+        console.log("Service worker: Scheduling Refreshing cache in ms:", CACHE_REFRESH_SCHEDULE_TIME);
+        const currentCacheManifest = await _getCurrentCacheManifest();
+        const newCacheManifest = await _getNewCacheManifest();
+        if(!newCacheManifest){
+            console.log("Service worker: could not fetch new cache manifest. Cache refresh will not be done.");
+            refreshInProgress = false;
+            return;
+        }
+        if(!currentCacheManifest && newCacheManifest){
+            console.log(`Service worker: Fresh install, writing cache manifest with ${Object.keys(newCacheManifest).length} entries`);
+            await _putCurrentCacheManifest(newCacheManifest);
+            refreshInProgress = false;
+            return;
+        }
+        await _fixCache(currentCacheManifest, newCacheManifest);
+        await _putCurrentCacheManifest(newCacheManifest);
+        event.ports[0].postMessage("cache refresh completed");
+    } catch (e) {
+        console.error("Service worker: error while refreshing cache", e);
+    }
+    refreshInProgress = false;
 }
 
 addEventListener('message', (event) => {
@@ -192,11 +279,11 @@ addEventListener('message', (event) => {
     }
 });
 
-function _isCacheableThirdPartyUrl(url) {
-    let THIRD_PARTY_URLS = [
+function _isCacheableExternalUrl(url) {
+    let EXTERNAL_URLS = [
         'https://storage.googleapis.com/workbox-cdn/'
     ];
-    for(let start of THIRD_PARTY_URLS){
+    for(let start of EXTERNAL_URLS){
         if(url.startsWith(start)){
             return true;
         }
@@ -204,10 +291,12 @@ function _isCacheableThirdPartyUrl(url) {
     return false;
 }
 
+// queue cache update
+
 const DONT_CACHE_BASE_URLS = [
     `${location.origin}/src/`, `${location.origin}/test/`, `${location.origin}/dist/`, // https://phcode.dev/src or other
     // https://phcode.dev/subfolder/src/ or other when phoenix is loaded from https://phcode.dev/subfolder/index.html
-    `${baseURL}src/`, `${baseURL}test/`, `${baseURL}dist/`];
+    `${baseURL}src/`, `${baseURL}test/`, `${baseURL}dist/`, `${baseURL}cacheManifest.json`];
 function _isNotCacheableUrl(url) {
     for(let start of DONT_CACHE_BASE_URLS){
         if(url.startsWith(start)){
@@ -217,7 +306,8 @@ function _isNotCacheableUrl(url) {
     return false;
 }
 
-// we always try to load main worker scripts and index html from core
+// we always try to load main worker scripts and index html from core scripts cache which uses stale while revalidate
+// to get aggressive updates.
 const CORE_SCRIPTS_URLS = [`${location.origin}/index.html`, `${location.origin}/`, // https://phcode.dev/src or other
     `${location.origin}/virtual-server-main.js`, `${location.origin}/phoenix/virtual-server-loader.js`,
     // https://phcode.dev/subfolder/src/ or other when phoenix is loaded from https://phcode.dev/subfolder/index.html
@@ -232,7 +322,7 @@ function _isCoreScript(url) {
     return false;
 }
 
-function _shouldCache(request) {
+function _belongsToEverythingCache(request) {
     // now do url checks, Remove # ,http://localhost:9000/dist/styles/images/sprites.svg#leftArrowDisabled.
     // we cache entries with query string parameters in static pages with base url starting with phoenix base
     let href = request.url.split("#")[0];
@@ -241,18 +331,19 @@ function _shouldCache(request) {
         return false;
     }
     if(_isNotCacheableUrl(href)){
-        _debugCacheLog("Not Caching un cacheable URL: ", request);
+        _debugCacheLog("Not Caching un cacheable URL in everything cache: ", request);
         return false;
     }
     if(_isCoreScript(href)){
-        _debugCacheLog("Not Caching core scripts: ", request);
+        _debugCacheLog("Not Caching core scripts in everything cache: ", request);
+        return false;
+    }
+    if(_isCacheableExternalUrl(href)){
+        _debugCacheLog("Not Caching external url in everything cache: ", request);
         return false;
     }
     let disAllowedExtensions =  /.zip$|.map$/i;
-    if(request.method === 'GET' && _isCacheableThirdPartyUrl(href)) {
-        return true;
-    }
-    if(request.method === 'GET' && href.startsWith(baseURL) && !disAllowedExtensions.test(href)) {
+    if(href.startsWith(baseURL) && !disAllowedExtensions.test(href)) {
         return true;
     }
     _debugCacheLog("Not Caching URL: ", request);
@@ -261,11 +352,8 @@ function _shouldCache(request) {
 
 // handle all document
 const allCachedRoutes = new Route(({ request }) => {
-    let shouldCache = _shouldCache(request) && !_isVirtualServing(request.url);
-    if(shouldCache){
-        recentlyAccessedURLS[request.url] = true;
-    }
-    return shouldCache;
+    return (request.method === 'GET'
+        && _belongsToEverythingCache(request) && !_isVirtualServing(request.url));
 }, new cacheFirst({
     cacheName: CACHE_NAME_EVERYTHING,
     plugins: [
@@ -278,7 +366,7 @@ const allCachedRoutes = new Route(({ request }) => {
 
 // core scripts route
 const freshnessPreferredRoutes = new Route(({ request }) => {
-    return _isCoreScript(request.url) && !_isVirtualServing(request.url);
+    return request.method === 'GET' && _isCoreScript(request.url) && !_isVirtualServing(request.url);
 }, new StaleWhileRevalidate({
     cacheName: CACHE_NAME_CORE_SCRIPTS,
     plugins: [
@@ -289,7 +377,21 @@ const freshnessPreferredRoutes = new Route(({ request }) => {
     ]
 }));
 
+// scripts with a different origin like third party libs
+const externalCachedRoutes = new Route(({ request }) => {
+    return request.method === 'GET' && _isCacheableExternalUrl(request.url) && !_isVirtualServing(request.url);
+}, new StaleWhileRevalidate({
+    cacheName: CACHE_NAME_EXTERNAL,
+    plugins: [
+        new ExpirationPlugin({
+            maxAgeSeconds: DAYS_30_IN_SEC,
+            purgeOnQuotaError: true
+        })
+    ]
+}));
+
 workbox.routing.registerRoute(allCachedRoutes);
 workbox.routing.registerRoute(freshnessPreferredRoutes);
+workbox.routing.registerRoute(externalCachedRoutes);
 
 workbox.core.clientsClaim();
