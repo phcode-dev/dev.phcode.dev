8a09f36ac9606d692cb33a00ed9f3f2092e1fb5f
phoenixide <phoenixide@users.noreply.github.com>
2022-09-10 17:21:00 +0000

    deploy: 851cfff8b3defac1e6c72e943951d28ef0d8865e

--- src/brackets.js
+++ src/brackets.js
@@ -66,6 +66,10 @@ define(function (require, exports, module) {
     require("thirdparty/CodeMirror/addon/selection/mark-selection");
     require("thirdparty/CodeMirror/keymap/sublime");
 
+    // Event dispatcher must be loaded before worker comm https://github.com/phcode-dev/phoenix/pull/678
+    require("utils/EventDispatcher");
+    require("worker/WorkerComm");
+
     // Load dependent modules
     const AppInit             = require("utils/AppInit"),
         LanguageManager     = require("language/LanguageManager"),
