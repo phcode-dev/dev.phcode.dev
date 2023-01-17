640916ed62f3f79309ddacd243f4704585c71c17
phoenixide <phoenixide@users.noreply.github.com>
2023-01-17 11:22:36 +0000

    deploy: 0b2f63921672a684d110735df22ff3c985f8a289

--- src/appConfig.js
+++ src/appConfig.js
@@ -21,7 +21,7 @@ window.AppConfig = {
         "extension_url": "https://brackets-archive.github.io/brackets-extensions/extensions/",
         "extension_store_url": "https://store.core.ai/src/",
         "linting.enabled_by_default": true,
-        "build_timestamp": "2023-01-17T10:26:53.138Z",
+        "build_timestamp": "2023-01-17T11:22:25.089Z",
         "googleAnalyticsID": "G-P4HJFPDB76",
         "mixPanelID": "49c4d164b592be2350fc7af06a259bf3",
         "coreAnalyticsID": "phoenix",
@@ -31,8 +31,8 @@ window.AppConfig = {
         "bugsnagEnv": "development"
     },
     "name": "Phoenix",
-    "version": "3.1.5-19124",
-    "apiVersion": "3.1.5",
+    "version": "3.1.6-19125",
+    "apiVersion": "3.1.6",
     "homepage": "https://core.ai",
     "issues": {
         "url": "https://github.com/phcode-dev/phoenix/issues"
