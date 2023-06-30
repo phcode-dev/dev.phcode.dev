26cd4e762c48647d334f4f4ce2f188359915c608
phoenixide <phoenixide@users.noreply.github.com>
2023-06-30 06:15:06 +0000

    deploy: cc3f7a7cfff356f456039ef535e302ae97e43c1e

--- src/appConfig.js
+++ src/appConfig.js
@@ -22,7 +22,7 @@ window.AppConfig = {
         "extension_url": "https://extensions.phcode.dev/extensions/",
         "extension_store_url": "https://store.core.ai/src/",
         "linting.enabled_by_default": true,
-        "build_timestamp": "2023-06-29T11:43:49.895Z",
+        "build_timestamp": "2023-06-30T06:14:56.059Z",
         "googleAnalyticsID": "G-P4HJFPDB76",
         "mixPanelID": "49c4d164b592be2350fc7af06a259bf3",
         "coreAnalyticsID": "phoenix",
@@ -32,7 +32,7 @@ window.AppConfig = {
         "bugsnagEnv": "development"
     },
     "name": "Phoenix",
-    "version": "3.2.2-19306",
+    "version": "3.2.2-19307",
     "apiVersion": "3.2.2",
     "homepage": "https://core.ai",
     "issues": {
