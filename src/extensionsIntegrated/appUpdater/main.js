935ca9d56162e6fbe205f40544203af7ff4be372
phoenixide <phoenixide@users.noreply.github.com>
2024-02-20 04:45:33 +0000

    deploy: 8a905e4ddcfcca9560160c6df0f62a027c3a7ba1

--- src/extensionsIntegrated/appUpdater/main.js
+++ src/extensionsIntegrated/appUpdater/main.js
@@ -1,19 +1,63 @@
+/*
+ * GNU AGPL-3.0 License
+ *
+ * Copyright (c) 2021 - present core.ai . All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see https://opensource.org/licenses/AGPL-3.0.
+ *
+ */
+
+/*global logger*/
+
 define(function (require, exports, module) {
     const AppInit = require("utils/AppInit"),
+        Metrics = require("utils/Metrics"),
         Commands = require("command/Commands"),
         CommandManager  = require("command/CommandManager"),
         Menus = require("command/Menus"),
         Dialogs = require("widgets/Dialogs"),
+        NodeUtils = require("utils/NodeUtils"),
         DefaultDialogs  = require("widgets/DefaultDialogs"),
         Strings     = require("strings"),
         marked = require('thirdparty/marked.min'),
         semver = require("thirdparty/semver.browser"),
+        TaskManager = require("features/TaskManager"),
         PreferencesManager  = require("preferences/PreferencesManager");
+    let updaterWindow, updateTask, updatePendingRestart, updateFailed;
 
-    const KEY_LAST_UPDATE_CHECK_TIME = "PH_LAST_UPDATE_CHECK_TIME",
+    const TAURI_UPDATER_WINDOW_LABEL = "updater",
+        KEY_LAST_UPDATE_CHECK_TIME = "PH_LAST_UPDATE_CHECK_TIME",
         KEY_UPDATE_AVAILABLE = "PH_UPDATE_AVAILABLE";
 
     function showOrHideUpdateIcon() {
+        if(!updaterWindow){
+            updaterWindow = window.__TAURI__.window.WebviewWindow.getByLabel(TAURI_UPDATER_WINDOW_LABEL);
+        }
+        if(updaterWindow && !updateTask) {
+            updateTask = TaskManager.addNewTask(Strings.UPDATING_APP, Strings.UPDATING_APP_MESSAGE,
+                `<i class="fa-solid fa-cogs"></i>`, {
+                    onSelect: function () {
+                        if(updatePendingRestart){
+                            Dialogs.showInfoDialog(Strings.UPDATE_READY_RESTART_TITLE, Strings.UPDATE_READY_RESTART_MESSAGE);
+                        } else if(updateFailed){
+                            Dialogs.showInfoDialog(Strings.UPDATE_FAILED_TITLE, Strings.UPDATE_FAILED_MESSAGE);
+                        }else {
+                            Dialogs.showInfoDialog(Strings.UPDATING_APP, Strings.UPDATING_APP_DIALOG_MESSAGE);
+                        }
+                    }
+                });
+        }
         let updateAvailable = PreferencesManager.getViewState(KEY_UPDATE_AVAILABLE);
         if(updateAvailable){
             $("#update-notification").removeClass("forced-hidden");
@@ -32,13 +76,33 @@ define(function (require, exports, module) {
             });
     }
 
-    async function doUpdate() {
-        const windowCount = await Phoenix.app.getPhoenixInstanceCount();
-        if(windowCount !== 1){
-            PreferencesManager.setViewState(KEY_UPDATE_AVAILABLE, true);
-            Dialogs.showInfoDialog(Strings.UPDATE_CLOSE_TO_UPDATE_TITLE, Strings.UPDATE_CLOSE_TO_UPDATE);
+    function createTauriUpdateWindow() {
+        if(updaterWindow){
             return;
         }
+        Metrics.countEvent(Metrics.EVENT_TYPE.UPDATES, 'window', "create"+Phoenix.platform);
+        // as we are a single instance app, and there can be multiple phoenix windows that comes in and goes out,
+        // the updater lives in its own independent hidden window.
+        updaterWindow = new window.__TAURI__.window.WebviewWindow(TAURI_UPDATER_WINDOW_LABEL, {
+            url: "tauri-updater.html",
+            title: "Desktop App Updater",
+            fullscreen: false,
+            resizable: false,
+            height: 320,
+            minHeight: 320,
+            width: 240,
+            minWidth: 240,
+            acceptFirstMouse: false,
+            visible: false
+        });
+        if(window.__TAURI__.window.WebviewWindow.getByLabel(TAURI_UPDATER_WINDOW_LABEL)){
+            Metrics.countEvent(Metrics.EVENT_TYPE.UPDATES, 'window', "okCreate"+Phoenix.platform);
+        }
+    }
+
+    async function doUpdate() {
+        createTauriUpdateWindow();
+        showOrHideUpdateIcon();
     }
 
     async function getUpdatePlatformKey() {
@@ -56,6 +120,7 @@ define(function (require, exports, module) {
         const updatePlatformKey = await getUpdatePlatformKey();
         const updateDetails = {
             shouldUpdate: false,
+            updatePendingRestart: false,
             downloadURL: null,
             currentVersion: Phoenix.metadata.apiVersion,
             updateVersion: null,
@@ -64,7 +129,9 @@ define(function (require, exports, module) {
         };
         try{
             const updateMetadata = await fetchJSON(brackets.config.app_update_url);
-            if(semver.gt(updateMetadata.version, Phoenix.metadata.apiVersion)){
+            const phoenixBinaryVersion = await NodeUtils.getPhoenixBinaryVersion();
+            const phoenixLoadedAppVersion = Phoenix.metadata.apiVersion;
+            if(semver.gt(updateMetadata.version, phoenixBinaryVersion)){
                 console.log("Update available: ", updateMetadata, "Detected platform: ", updatePlatformKey);
                 PreferencesManager.setViewState(KEY_UPDATE_AVAILABLE, true);
                 updateDetails.shouldUpdate = true;
@@ -73,6 +140,11 @@ define(function (require, exports, module) {
                 if(updateMetadata.platforms && updateMetadata.platforms[updatePlatformKey]){
                     updateDetails.downloadURL = updateMetadata.platforms[updatePlatformKey].url;
                 }
+            } else if(semver.eq(updateMetadata.version, phoenixBinaryVersion) &&
+                !semver.eq(phoenixLoadedAppVersion, phoenixBinaryVersion)){
+                console.log("Updates applied, waiting for app restart: ", phoenixBinaryVersion, phoenixLoadedAppVersion);
+                updateDetails.updatePendingRestart = true;
+                PreferencesManager.setViewState(KEY_UPDATE_AVAILABLE, true);
             } else {
                 console.log("no updates available for platform: ", updateDetails.updatePlatform);
                 PreferencesManager.setViewState(KEY_UPDATE_AVAILABLE, false);
@@ -86,7 +158,19 @@ define(function (require, exports, module) {
 
     async function checkForUpdates(isAutoUpdate) {
         showOrHideUpdateIcon();
+        if(updateTask){
+            $("#status-tasks .btn-dropdown").click();
+            return;
+        }
         const updateDetails = await getUpdateDetails();
+        if(updateFailed) {
+            (!isAutoUpdate) && Dialogs.showInfoDialog(Strings.UPDATE_FAILED_TITLE, Strings.UPDATE_FAILED_MESSAGE);
+            return;
+        }
+        if(updatePendingRestart || updateDetails.updatePendingRestart){
+            (!isAutoUpdate) && Dialogs.showInfoDialog(Strings.UPDATE_READY_RESTART_TITLE, Strings.UPDATE_READY_RESTART_MESSAGE);
+            return;
+        }
         if(!updateDetails.shouldUpdate){
             (!isAutoUpdate) && Dialogs.showInfoDialog(Strings.UPDATE_NOT_AVAILABLE_TITLE, Strings.UPDATE_UP_TO_DATE);
             return;
@@ -95,30 +179,89 @@ define(function (require, exports, module) {
             { className: Dialogs .DIALOG_BTN_CLASS_NORMAL, id: Dialogs .DIALOG_BTN_CANCEL, text: Strings.UPDATE_LATER },
             { className: Dialogs .DIALOG_BTN_CLASS_PRIMARY, id: Dialogs .DIALOG_BTN_OK, text: Strings.GET_IT_NOW }
         ];
-        let markdownHtml = marked.parse(updateDetails.releaseNotesMarkdown);
+        let markdownHtml = marked.parse(updateDetails.releaseNotesMarkdown || "");
+        Metrics.countEvent(Metrics.EVENT_TYPE.UPDATES, 'dialog', "shown"+Phoenix.platform);
         Dialogs.showModalDialog(DefaultDialogs.DIALOG_ID_INFO, Strings.UPDATE_AVAILABLE_TITLE, markdownHtml, buttons)
             .done(option=>{
-                if(option === Dialogs.DIALOG_BTN_OK){
+                if(option === Dialogs.DIALOG_BTN_OK && !updaterWindow){
                     doUpdate();
+                    return;
                 }
+                Metrics.countEvent(Metrics.EVENT_TYPE.UPDATES, 'dialog', "cancel"+Phoenix.platform);
             });
     }
 
+    const UPDATE_COMMANDS = {
+        GET_STATUS: "GET_STATUS"
+    };
+    const UPDATE_EVENT = {
+        STATUS: "STATUS",
+        LOG_ERROR: "LOG_ERROR"
+    };
+    const UPDATE_STATUS = {
+        STARTED: "STARTED",
+        FAILED: "FAILED",
+        FAILED_UNKNOWN_OS: "FAILED_UNKNOWN_OS",
+        INSTALLED: "INSTALLED"
+    };
+
+    function _sendUpdateCommand(command, data) {
+        window.__TAURI__.event.emit('updateCommands', {command, data});
+    }
+
+    function _refreshUpdateStatus() {
+        _sendUpdateCommand(UPDATE_COMMANDS.GET_STATUS);
+    }
+
+    let updateInstalledDialogShown = false, updateFailedDialogShown = false;
     AppInit.appReady(function () {
         if(!Phoenix.browser.isTauri || Phoenix.isTestWindow) {
             // app updates are only for desktop builds
             return;
         }
+        updaterWindow = window.__TAURI__.window.WebviewWindow.getByLabel(TAURI_UPDATER_WINDOW_LABEL);
+        window.__TAURI__.event.listen("updater-event", (receivedEvent)=> {
+            console.log("received Event updater-event", receivedEvent);
+            const {eventName, data} = receivedEvent.payload;
+            if(eventName === UPDATE_EVENT.STATUS) {
+                if(data === UPDATE_STATUS.FAILED_UNKNOWN_OS && !updateFailedDialogShown){
+                    updateFailedDialogShown = true;
+                    Metrics.countEvent(Metrics.EVENT_TYPE.UPDATES, 'fail', "Unknown"+Phoenix.platform);
+                    updateFailed = true;
+                    updateTask.setFailed();
+                    updateTask.setMessage(Strings.UPDATE_FAILED_TITLE);
+                } else if(data === UPDATE_STATUS.FAILED && !updateFailedDialogShown){
+                    updateFailedDialogShown = true;
+                    Metrics.countEvent(Metrics.EVENT_TYPE.UPDATES, 'fail', Phoenix.platform);
+                    updateFailed = true;
+                    updateTask.setFailed();
+                    updateTask.setMessage(Strings.UPDATE_FAILED_TITLE);
+                    Dialogs.showInfoDialog(Strings.UPDATE_FAILED_TITLE, Strings.UPDATE_FAILED_MESSAGE);
+                } else if(data === UPDATE_STATUS.INSTALLED && !updateInstalledDialogShown){
+                    updateInstalledDialogShown = true;
+                    Metrics.countEvent(Metrics.EVENT_TYPE.UPDATES, 'done', Phoenix.platform);
+                    updatePendingRestart = true;
+                    updateTask.setSucceded();
+                    updateTask.setTitle(Strings.UPDATE_DONE);
+                    updateTask.setMessage(Strings.UPDATE_RESTART);
+                    Dialogs.showInfoDialog(Strings.UPDATE_READY_RESTART_TITLE, Strings.UPDATE_READY_RESTART_MESSAGE);
+                }
+                showOrHideUpdateIcon();
+            } if(eventName === UPDATE_EVENT.LOG_ERROR) {
+                logger.reportErrorMessage(data);
+            }
+        });
         $("#update-notification").click(()=>{
             checkForUpdates();
         });
-        const commandID = Commands.HELP_CHECK_UPDATES || "help.checkUpdates";// todo remove this line after dev
+        const commandID = Commands.HELP_CHECK_UPDATES;
         CommandManager.register(Strings.CMD_CHECK_FOR_UPDATE, commandID, ()=>{
             checkForUpdates();
         });
         const helpMenu = Menus.getMenu(Menus.AppMenuBar.HELP_MENU);
         helpMenu.addMenuItem(commandID, "", Menus.AFTER, Commands.HELP_GET_INVOLVED);
         showOrHideUpdateIcon();
+        _refreshUpdateStatus();
         // check for updates at boot
         let lastUpdateCheckTime = PreferencesManager.getViewState(KEY_LAST_UPDATE_CHECK_TIME);
         if(!lastUpdateCheckTime){
