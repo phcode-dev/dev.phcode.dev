f9d00edf3163b3987b8e1ae71de9e8f57810ce4f
phoenixide <phoenixide@users.noreply.github.com>
2024-02-26 05:23:33 +0000

    deploy: 8a905e4ddcfcca9560160c6df0f62a027c3a7ba1

--- appConfig.js
+++ appConfig.js
@@ -25,7 +25,7 @@ window.AppConfig = {
         "app_notification_url": "assets/notifications/dev/",
         "app_update_url": "https://updates.phcode.io/tauri/update-latest-experimental-build.json",
         "linting.enabled_by_default": true,
-        "build_timestamp": "2024-02-25T11:00:04.316Z",
+        "build_timestamp": "2024-02-26T05:23:22.231Z",
         "googleAnalyticsID": "G-P4HJFPDB76",
         "googleAnalyticsIDDesktop": "G-VE5BXWJ0HF",
         "mixPanelID": "49c4d164b592be2350fc7af06a259bf3",
@@ -37,7 +37,7 @@ window.AppConfig = {
         "bugsnagEnv": "development"
     },
     "name": "Phoenix Code",
-    "version": "3.4.3-19941",
+    "version": "3.4.3-19944",
     "apiVersion": "3.4.3",
     "homepage": "https://core.ai",
     "issues": {
