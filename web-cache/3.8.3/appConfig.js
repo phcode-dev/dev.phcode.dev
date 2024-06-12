6a32af2e43ace9e932e763bdc835e0bc32a677ea
phoenixide <phoenixide@users.noreply.github.com>
2024-06-12 14:49:00 +0000

    deploy: 606b7d045a60ae2f4fb4379b29619432e49cf184

--- web-cache/3.8.3/appConfig.js
+++ web-cache/3.8.3/appConfig.js
@@ -26,7 +26,7 @@ window.AppConfig = {
         "app_notification_url": "assets/notifications/dev/",
         "app_update_url": "https://updates.phcode.io/tauri/update-latest-experimental-build.json",
         "linting.enabled_by_default": true,
-        "build_timestamp": "2024-06-12T14:48:04.495Z",
+        "build_timestamp": "2024-06-12T14:48:45.642Z",
         "googleAnalyticsID": "G-P4HJFPDB76",
         "googleAnalyticsIDDesktop": "G-VE5BXWJ0HF",
         "mixPanelID": "49c4d164b592be2350fc7af06a259bf3",
