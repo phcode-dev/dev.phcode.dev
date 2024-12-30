f1ebda1a267cffca2f73752c52d6312aeb201f41
phoenixide <phoenixide@users.noreply.github.com>
2024-12-30 05:19:10 +0000

    deploy: 606b7d045a60ae2f4fb4379b29619432e49cf184

--- web-cache/3.11.0/brackets-min.js
+++ web-cache/3.11.0/brackets-min.js
@@ -16296,6 +16296,9 @@ define("document/DocumentCommandHandlers", function (require, exports, module) {
         if (entry) {
             brackets.app.openPathInFileBrowser(entry.fullPath)
                 .catch(err=>console.error("Error showing '" + entry.fullPath + "' in OS folder:", err));
+        } else {
+            brackets.app.openPathInFileBrowser(ProjectManager.getProjectRoot().fullPath)
+                .catch(err=>console.error("Error showing '" + ProjectManager.getProjectRoot().fullPath + "' in OS folder:", err));
         }
     }
 
@@ -16303,6 +16306,8 @@ define("document/DocumentCommandHandlers", function (require, exports, module) {
         const entry = ProjectManager.getSelectedItem();
         if (entry && entry.fullPath) {
             NodeUtils.openNativeTerminal(entry.fullPath);
+        } else {
+            NodeUtils.openNativeTerminal(ProjectManager.getProjectRoot().fullPath);
         }
     }
 
@@ -16310,13 +16315,17 @@ define("document/DocumentCommandHandlers", function (require, exports, module) {
         const entry = ProjectManager.getSelectedItem();
         if (entry && entry.fullPath) {
             NodeUtils.openNativeTerminal(entry.fullPath, true);
+        } else {
+            NodeUtils.openNativeTerminal(ProjectManager.getProjectRoot().fullPath, true);
         }
     }
 
     function openDefaultApp() {
         const entry = ProjectManager.getSelectedItem();
         if (entry && entry.fullPath) {
-            NodeUtils.openInDefaultApp(entry.fullPath, true);
+            NodeUtils.openInDefaultApp(entry.fullPath);
+        } else {
+            NodeUtils.openInDefaultApp(ProjectManager.getProjectRoot().fullPath);
         }
     }
 
@@ -147661,28 +147670,31 @@ define("utils/LocalizationUtils", function (require, exports, module) {
      * @param {Date} [date] - The date to compare with the current date and time. If not given, defaults to now.
      * @param {string} [lang] - Optional language code to use for formatting (e.g., 'en', 'fr').
      *                          If not provided, defaults to the application locale or 'en'.
+     * @param {Date} [fromDate] - Optional date to use instead of now to compute the relative dateTime from.
      * @returns {string} - A human-readable relative time string (e.g., "2 days ago", "in 3 hours").
      */
-    function dateTimeFromNow(date, lang) {
+    function dateTimeFromNow(date, lang, fromDate) {
         date = date || new Date();
-        const now = new Date();
-        const diffInSeconds = Math.floor((date - now) / 1000);
+        fromDate = fromDate || new Date();
+        const diffInSeconds = Math.floor((date - fromDate) / 1000);
 
         const rtf = new Intl.RelativeTimeFormat([lang || brackets.getLocale() || "en", "en"],
             { numeric: 'auto' });
-
-        if (Math.abs(diffInSeconds) < 60) {
+        if (Math.abs(diffInSeconds) < 3) {
+            // we consider diffs less than 3 seconds to be always 'now', for better UX and for unit tests stability.
+            return rtf.format(0, 'second');
+        } else if (Math.abs(diffInSeconds) < 60) {
             return rtf.format(diffInSeconds, 'second');
         } else if (Math.abs(diffInSeconds) < 3600) {
-            return rtf.format(Math.floor(diffInSeconds / 60), 'minute');
+            return rtf.format(Math.trunc(diffInSeconds / 60), 'minute');
         } else if (Math.abs(diffInSeconds) < 86400) {
-            return rtf.format(Math.floor(diffInSeconds / 3600), 'hour');
+            return rtf.format(Math.trunc(diffInSeconds / 3600), 'hour');
         } else if (Math.abs(diffInSeconds) < 2592000) {
-            return rtf.format(Math.floor(diffInSeconds / 86400), 'day');
+            return rtf.format(Math.trunc(diffInSeconds / 86400), 'day');
         } else if (Math.abs(diffInSeconds) < 31536000) {
-            return rtf.format(Math.floor(diffInSeconds / 2592000), 'month');
+            return rtf.format(Math.trunc(diffInSeconds / 2592000), 'month');
         } else {
-            return rtf.format(Math.floor(diffInSeconds / 31536000), 'year');
+            return rtf.format(Math.trunc(diffInSeconds / 31536000), 'year');
         }
     }
 
@@ -147699,7 +147711,7 @@ define("utils/LocalizationUtils", function (require, exports, module) {
     function dateTimeFromNowFriendly(date, lang) {
         const now = new Date();
         const diffInMilliseconds = date - now;
-        const diffInDays = Math.floor(diffInMilliseconds / (1000 * 60 * 60 * 24));
+        const diffInDays = Math.trunc(diffInMilliseconds / (1000 * 60 * 60 * 24));
 
         // If within the last 30 days or the future, use relative time
         if (Math.abs(diffInDays) <= 30) {
