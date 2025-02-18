5c96c520710a55df5fc75c97612705f292a2d861
phoenixide <phoenixide@users.noreply.github.com>
2025-02-18 10:53:15 +0000

    deploy: 606b7d045a60ae2f4fb4379b29619432e49cf184

--- brackets-min.js
+++ brackets-min.js
@@ -121,6 +121,7 @@ define(function (require, exports, module) {
     // load modules for later use
     require("utils/Global");
     require("editor/CSSInlineEditor");
+    require("preferences/AllPreferences");
     require("project/WorkingSetSort");
     require("search/QuickOpen");
     require("search/QuickOpenHelper");
@@ -101855,6 +101856,9 @@ define("nls/root/strings", {
     "DESCRIPTION_HIDE_FIRST": "true to show the first Indent Guide line else false.",
     "DESCRIPTION_CSS_COLOR_PREVIEW": "true to display color previews in the gutter, else false.",
 
+    // Emmet
+    "DESCRIPTION_EMMET": "true to enable Emmet, else false.",
+
     // Git extension
     "ENABLE_GIT": "Enable Git",
     "ACTION": "Action",
@@ -102129,7 +102133,6 @@ define("nls/root/strings", {
     // surveys
     "SURVEY_TITLE_VOTE_FOR_FEATURES_YOU_WANT": "Vote for the features you want to see next!"
 });
-
 /*
  * GNU AGPL-3.0 License
  *
@@ -134608,6 +134611,56 @@ $3132870559d60e53$require$initFsLib($8adf1cfaed2eb5b1$exports);
 })();
 //# sourceMappingURL=virtualfs.js.map
 
+/*
+ * GNU AGPL-3.0 License
+ *
+ * Copyright (c) 2021 - present core.ai . All rights reserved.
+ * Original work Copyright (c) 2012 - 2021 Adobe Systems Incorporated. All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see https://opensource.org/licenses/AGPL-3.0.
+ *
+ */
+
+/*
+ * This file houses all the preferences used across Phoenix.
+ *
+ * To use:
+ * ```
+ * const AllPreferences = brackets.getModule("preferences/AllPreferences");
+ * function preferenceChanged() {
+       enabled = PreferencesManager.get(AllPreferences.EMMET);
+   }
+ * PreferencesManager.on("change", AllPreferences.EMMET, preferenceChanged);
+   preferenceChanged();
+ * ```
+ */
+
+   define("preferences/AllPreferences", function (require, exports, module) {
+    const PreferencesManager = require("preferences/PreferencesManager");
+    const Strings = require("strings");
+
+    // list of all the preferences
+    const PREFERENCES_LIST = {
+        EMMET: "emmet"
+    };
+
+    PreferencesManager.definePreference(PREFERENCES_LIST.EMMET, "boolean", true, {
+        description: Strings.DESCRIPTION_EMMET
+    });
+
+    module.exports = PREFERENCES_LIST;
+});
 /*
  * GNU AGPL-3.0 License
  *
