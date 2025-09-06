969d85521c82891ad544fbaf259ed8030bedb415
phoenixide <phoenixide@users.noreply.github.com>
2025-09-06 09:41:51 +0000

    deploy: 606b7d045a60ae2f4fb4379b29619432e49cf184

--- src/extensionsIntegrated/TabBar/main.js
+++ src/extensionsIntegrated/TabBar/main.js
@@ -229,6 +229,7 @@ define(function (require, exports, module) {
             $tabBar = $(TabBarHTML);
             // since we need to add the tab bar before the editor which has .not-editor class
             $(".pane-header").after($tabBar);
+            $("#overflow-button").attr("title", Strings.TABBAR_SHOW_HIDDEN_TABS);
             WorkspaceManager.recomputeLayout(true);
             updateTabs();
         } else if ($paneHeader.length === 2) {
@@ -239,6 +240,8 @@ define(function (require, exports, module) {
             // TODO: Fix bug where the tab bar gets hidden inside the editor in horizontal split
             $paneHeader.eq(0).after($tabBar);
             $paneHeader.eq(1).after($tabBar2);
+            $("#overflow-button").attr("title", Strings.TABBAR_SHOW_HIDDEN_TABS);
+            $("#overflow-button-2").attr("title", Strings.TABBAR_SHOW_HIDDEN_TABS);
             WorkspaceManager.recomputeLayout(true);
             updateTabs();
         }
@@ -487,57 +490,36 @@ define(function (require, exports, module) {
     function handleTabClick() {
         // delegate event handling for both tab bars
         $(document).on("click", ".phoenix-tab-bar .tab", function (event) {
+            // Get the file path from the data-path attribute of the parent tab
+            const filePath = $(this).attr("data-path");
+            if (!filePath) { return; }
+
+            // determine the pane inside which the tab belongs
+            const isSecondPane = $(this).closest("#phoenix-tab-bar-2").length > 0;
+            const paneId = isSecondPane ? "second-pane" : "first-pane";
+
+            // get the file object
+            const fileObj = FileSystem.getFileForPath(filePath);
+
             // check if the clicked element is the close button
             if ($(event.target).hasClass("fa-times") || $(event.target).closest(".tab-close").length) {
-                // Get the file path from the data-path attribute of the parent tab
-                const filePath = $(this).attr("data-path");
+                event.preventDefault();
+                event.stopPropagation();
 
-                if (filePath) {
-                    // determine the pane inside which the tab belongs
-                    const isSecondPane = $(this).closest("#phoenix-tab-bar-2").length > 0;
-                    const paneId = isSecondPane ? "second-pane" : "first-pane";
-
-                    // get the file object
-                    const fileObj = FileSystem.getFileForPath(filePath);
-                    // close the file
-                    CommandManager.execute(Commands.FILE_CLOSE, { file: fileObj, paneId: paneId });
-
-                    // Prevent default behavior
-                    event.preventDefault();
-                    event.stopPropagation();
-                }
-            }
-        });
-
-        // delegate event handling for both tab bars
-        $(document).on("mousedown", ".phoenix-tab-bar .tab", function (event) {
-            if ($(event.target).hasClass("fa-times") || $(event.target).closest(".tab-close").length) {
-                return;
-            }
-            // Get the file path from the data-path attribute
-            const filePath = $(this).attr("data-path");
-
-            if (filePath) {
-                // determine the pane inside which the tab belongs
-                const isSecondPane = $(this).closest("#phoenix-tab-bar-2").length > 0;
-                const paneId = isSecondPane ? "second-pane" : "first-pane";
+                CommandManager.execute(Commands.FILE_CLOSE, { file: fileObj, paneId: paneId }); // close the file
+            } else { // open the clicked tab
                 const currentActivePane = MainViewManager.getActivePaneId();
                 const isPaneActive = paneId === currentActivePane;
                 const currentFile = MainViewManager.getCurrentlyViewedFile(currentActivePane);
 
-                // Check if this is a placeholder tab
+                // if the clicked tab is a placeholder tab, we add it to the working set
                 if ($(this).hasClass("placeholder")) {
-                    // Add the file to the working set when placeholder tab is clicked
-                    const fileObj = FileSystem.getFileForPath(filePath);
                     MainViewManager.addToWorkingSet(paneId, fileObj);
                 }
 
-                if (isPaneActive && currentFile && currentFile.fullPath === filePath) {
-                    return;
-                }
+                // clicked tab is already active, don't do anything
+                if (isPaneActive && currentFile && currentFile.fullPath === filePath) { return; }
                 CommandManager.execute(Commands.FILE_OPEN, { fullPath: filePath, paneId: paneId });
-
-                // We dont prevent default behavior here to enable drag and drop of this tab
             }
         });
 
@@ -613,7 +595,7 @@ define(function (require, exports, module) {
         MainViewManager.on("paneCreate paneDestroy paneLayoutChange", createTabBar);
 
         // For active pane changes, update only the tabs
-        MainViewManager.on("activePaneChange", updateTabs);
+        MainViewManager.on("activePaneChange", debounceUpdateTabs);
 
         // For editor changes, update only the tabs.
         MainViewManager.on(MainViewManager.EVENT_CURRENT_FILE_CHANGE, debounceUpdateTabs);
