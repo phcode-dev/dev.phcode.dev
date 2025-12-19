4ed5ea5e80d284eca7e2af2dd8d3f14366435ee7
phoenixide <phoenixide@users.noreply.github.com>
2025-12-19 03:18:28 +0000

    deploy: cd657c31c72637043653fd55e8dd585d7c158b79

--- assets/default-project/en/script.js
+++ assets/default-project/en/script.js
@@ -1,14 +1,1 @@
-function onPageLoaded() {
-    // Write your javascript code here
-    console.log("page loaded");
-}
-
-document.addEventListener("DOMContentLoaded", function () {
-    // Listen for clicks on elements with the class 'play-button'
-    document.querySelectorAll(".play-button").forEach(function (button) {
-        button.addEventListener("click", function () {
-            // When a play button is clicked, simulate a click on the <a> tag within the same .video-container
-            this.parentNode.querySelector("a").click();
-        });
-    });
-});
+function onPageLoaded(){console.log("page loaded")}document.addEventListener("DOMContentLoaded",function(){document.querySelectorAll(".play-button").forEach(function(button){button.addEventListener("click",function(){this.parentNode.querySelector("a").click()})})});
