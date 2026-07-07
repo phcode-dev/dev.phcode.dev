ed6c0815e691b159f00124c8b044239dd675522e
phoenixide <phoenixide@users.noreply.github.com>
2026-07-07 19:43:16 +0000

    deploy: cd657c31c72637043653fd55e8dd585d7c158b79

--- web-cache/5.2.0/appConfig.js
+++ web-cache/5.2.0/appConfig.js
@@ -35,7 +35,7 @@ window.AppConfig = {
         "app_update_url": "https://updates.phcode.io/tauri/update-latest-pre-release.json",
         "extensionTakedownURL": "https://updates.phcode.io/extension_takedown.json",
         "linting.enabled_by_default": true,
-        "build_timestamp": "2026-07-07T17:55:29.336Z",
+        "build_timestamp": "2026-07-07T19:43:01.225Z",
         "googleAnalyticsID": "G-FP5S9BKDSJ",
         "googleAnalyticsIDDesktop": "G-D5R1Y6PTS8",
         "mixPanelID": "a7e08ffd43c37767c29b13df1d2e6c62",
@@ -47,7 +47,7 @@ window.AppConfig = {
         "bugsnagEnv": "staging"
     },
     "name": "Phoenix Code",
-    "version": "5.2.0-23135",
+    "version": "5.2.0-23136",
     "apiVersion": "5.2.0",
     "homepage": "https://core.ai",
     "issues": {
