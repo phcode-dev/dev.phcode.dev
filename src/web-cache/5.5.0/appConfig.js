ac30d51aee9003d5c5bf397d54b7ed6aab6d2d32
phoenixide <phoenixide@users.noreply.github.com>
2026-08-27 20:08:53 +0000

    deploy: cd657c31c72637043653fd55e8dd585d7c158b79

--- src/web-cache/5.5.0/appConfig.js
+++ src/web-cache/5.5.0/appConfig.js
@@ -40,7 +40,7 @@ window.AppConfig = {
             "ruff": "0.15.20"
         },
         "linting.enabled_by_default": true,
-        "build_timestamp": "2026-08-27T19:31:37.559Z",
+        "build_timestamp": "2026-08-27T20:08:39.418Z",
         "googleAnalyticsID": "G-FP5S9BKDSJ",
         "googleAnalyticsIDDesktop": "G-D5R1Y6PTS8",
         "mixPanelID": "a7e08ffd43c37767c29b13df1d2e6c62",
@@ -52,7 +52,7 @@ window.AppConfig = {
         "bugsnagEnv": "staging"
     },
     "name": "Phoenix Code",
-    "version": "5.5.0-23354",
+    "version": "5.5.0-23355",
     "apiVersion": "5.5.0",
     "homepage": "https://core.ai",
     "issues": {
